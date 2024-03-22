605fc53da900fc737241030e0e401abc502e1294
Moreno Cabral Irvin Alonso <143743685+MorenoIA128@users.noreply.github.com>
2024-03-22 12:53:41 -0600

    Update pantallainicial_0393.dart

--- lib/pantallainicial_0393.dart
+++ lib/pantallainicial_0393.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 //PantallaInicial_0393
@@ -8,25 +10,160 @@ class PantallaInicial_0393 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Pantalla Inicial Irvin Moreno0393"),
-        backgroundColor: Colors.indigo,
+        title: const Text(
+          "Pantalla Inicial Irvin Moreno0393",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0xff643fb5),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
               onPressed: () {
                 Navigator.pushNamed(context, "/Pantalla1_0393");
               }, // fin de onPressed
-              child: Text("Mover a Pantalla1"),
+              child: const Text("Zona de aterrizaje p1",
+                  style: TextStyle(color: Colors.white)),
             ),
             ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
               onPressed: () {
                 Navigator.pushNamed(context, "/Pantalla2_0393");
               }, // fin de onPressed
-              child: Text("Mover a Pantalla2"),
-            )
+              child: const Text("Encabezado p2",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla3_0393");
+              }, // fin de onPressed
+              child: const Text("Cargando p3",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla4_0393");
+              }, // fin de onPressed
+              child:
+                  const Text("Reto p4", style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla5_0393");
+              }, // fin de onPressed
+              child: const Text("Texto abajo container p5",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla6_0393");
+              }, // fin de onPressed
+              child: const Text("Texto centrado p6",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla7_0393");
+              }, // fin de onPressed
+              child: const Text("Contenedor redondeado p7",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla8_0393");
+              }, // fin de onPressed
+              child: const Text("Algunas esquinas redondeadas p8",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla9_0393");
+              }, // fin de onPressed
+              child: const Text("Circulo p9",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla10_0393");
+              }, // fin de onPressed
+              child: const Text("Contenedor borde p10",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla11_0393");
+              }, // fin de onPressed
+              child: const Text("Contenedor sombra p11",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla12_0393");
+              }, // fin de onPressed
+              child: const Text("Contenedor difuminado p12",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla13_0393");
+              }, // fin de onPressed
+              child: const Text("Contenedores p13",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla14_0393");
+              }, // fin de onPressed
+              child: const Text("Widget hijo p14",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla15_0393");
+              }, // fin de onPressed
+              child: const Text("Widget hijo no define su altura p15",
+                  style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Color(0xca6d6ab7)),
+              onPressed: () {
+                Navigator.pushNamed(context, "/Pantalla16_0393");
+              }, // fin de onPressed
+              child: const Text("Texto arrriba container p16",
+                  style: TextStyle(color: Colors.white)),
+            ),
           ], // niños widgets
         ),
       ),
